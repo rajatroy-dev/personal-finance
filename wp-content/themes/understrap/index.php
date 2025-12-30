8ca2a85b861713a7eeb1882dea834295e64c646b
Rajat Roy <rajatroy.dev@gmail.com>
2025-12-30 12:26:30 +0530

    WIP Homepage

--- wp-content/themes/understrap/index.php
+++ wp-content/themes/understrap/index.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The main template file
  *
@@ -12,59 +13,115 @@
  */
 
 // Exit if accessed directly.
-defined( 'ABSPATH' ) || exit;
+defined('ABSPATH') || exit;
 
 get_header();
 
-$container = get_theme_mod( 'understrap_container_type' );
+$container = get_theme_mod('understrap_container_type');
 ?>
 
-<?php if ( is_front_page() && is_home() ) : ?>
-	<?php get_template_part( 'global-templates/hero' ); ?>
+<?php if (is_front_page() && is_home()) : ?>
+	<?php get_template_part('global-templates/hero'); ?>
 <?php endif; ?>
 
 <div class="wrapper" id="index-wrapper">
 
-	<div class="<?php echo esc_attr( $container ); ?>" id="content" tabindex="-1">
+	<div class="<?php echo esc_attr($container); ?>" id="content" tabindex="-1">
 
-		<div class="row">
+		<section class="my-3">
+			<ul class="nav nav-tabs" id="myTab" role="tablist">
+				<li class="nav-item" role="presentation">
+					<button class="nav-link active" id="home-tab" data-bs-toggle="tab" data-bs-target="#home-tab-pane"
+						type="button" role="tab" aria-controls="home-tab-pane" aria-selected="true">Investments</button>
+				</li>
+				<li class="nav-item" role="presentation">
+					<button class="nav-link" id="profile-tab" data-bs-toggle="tab" data-bs-target="#profile-tab-pane"
+						type="button" role="tab" aria-controls="profile-tab-pane" aria-selected="false">Savings</button>
+				</li>
+			</ul>
+			<div class="tab-content" id="myTabContent">
+				<div class="tab-pane fade show active" id="home-tab-pane" role="tabpanel" aria-labelledby="home-tab"
+					tabindex="0">
 
-			<?php
-			// Do the left sidebar check and open div#primary.
-			get_template_part( 'global-templates/left-sidebar-check' );
-			?>
+					<div class="d-flex justify-content-end my-3">
+						<a href="#" class="btn btn-primary w-100">VIEW IN DETAILS</a>
+					</div>
 
-			<main class="site-main" id="main">
+					<div class="table-responsive my-3">
+						<table class="table">
+							<thead>
+								<tr>
+									<th scope="col">Record</th>
+									<th scope="col">Balance</th>
+									<th scope="col">Credit</th>
+									<th scope="col">Debit</th>
+								</tr>
+							</thead>
+							<tbody>
+								<tr>
+									<th scope="row">1</th>
+									<td>₹10,000.00</td>
+									<td>₹10,000.00</td>
+									<td>₹10,000.00</td>
+								</tr>
+								<tr>
+									<th scope="row">2</th>
+									<td>₹10,000.00</td>
+									<td>₹10,000.00</td>
+									<td>₹10,000.00</td>
+								</tr>
+								<tr>
+									<th scope="row">3</th>
+									<td>₹10,000.00</td>
+									<td>₹10,000.00</td>
+									<td>₹10,000.00</td>
+								</tr>
+							</tbody>
+						</table>
+					</div>
+				</div>
+				<div class="tab-pane fade" id="profile-tab-pane" role="tabpanel" aria-labelledby="profile-tab"
+					tabindex="0">
 
-				<?php
-				if ( have_posts() ) {
-					// Start the Loop.
-					while ( have_posts() ) {
-						the_post();
+					<div class="d-flex justify-content-end my-3">
+						<a href="#" class="btn btn-primary w-100">VIEW IN DETAILS</a>
+					</div>
 
-						/*
-						 * Include the Post-Format-specific template for the content.
-						 * If you want to override this in a child theme, then include a file
-						 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-						 */
-						get_template_part( 'loop-templates/content', get_post_format() );
-					}
-				} else {
-					get_template_part( 'loop-templates/content', 'none' );
-				}
-				?>
-
-			</main>
-
-			<?php
-			// Display the pagination component.
-			understrap_pagination();
-
-			// Do the right sidebar check and close div#primary.
-			get_template_part( 'global-templates/right-sidebar-check' );
-			?>
-
-		</div><!-- .row -->
+					<div class="table-responsive my-3">
+						<table class="table">
+							<thead>
+								<tr>
+									<th scope="col">Record</th>
+									<th scope="col">Balance</th>
+									<th scope="col">Credit</th>
+									<th scope="col">Debit</th>
+								</tr>
+							</thead>
+							<tbody>
+								<tr>
+									<th scope="row">1</th>
+									<td>₹10,000.00</td>
+									<td>₹10,000.00</td>
+									<td>₹10,000.00</td>
+								</tr>
+								<tr>
+									<th scope="row">2</th>
+									<td>₹10,000.00</td>
+									<td>₹10,000.00</td>
+									<td>₹10,000.00</td>
+								</tr>
+								<tr>
+									<th scope="row">3</th>
+									<td>₹10,000.00</td>
+									<td>₹10,000.00</td>
+									<td>₹10,000.00</td>
+								</tr>
+							</tbody>
+						</table>
+					</div>
+				</div>
+			</div>
+		</section>
 
 	</div><!-- #content -->
 
