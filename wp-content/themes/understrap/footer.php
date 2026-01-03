fbf947a1be818a41106466ce6304aa0a3fa5fa55
Rajat Roy <rajatroy.dev@gmail.com>
2026-01-03 12:45:47 +0530

    WIP Footer

--- wp-content/themes/understrap/footer.php
+++ wp-content/themes/understrap/footer.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The template for displaying the footer
  *
@@ -8,28 +9,26 @@
  */
 
 // Exit if accessed directly.
-defined( 'ABSPATH' ) || exit;
+defined('ABSPATH') || exit;
 
-$container = get_theme_mod( 'understrap_container_type' );
+$container = get_theme_mod('understrap_container_type');
 ?>
 
-<?php get_template_part( 'sidebar-templates/sidebar', 'footerfull' ); ?>
+<?php get_template_part('sidebar-templates/sidebar', 'footerfull'); ?>
 
-<div class="wrapper" id="wrapper-footer">
+<div class="wrapper bg-light fixed-bottom py-2" id="wrapper-footer">
 
-	<div class="<?php echo esc_attr( $container ); ?>">
+	<div class="<?php echo esc_attr($container); ?>">
 
 		<div class="row">
 
-			<div class="col-md-12">
+			<div class="col">
 
 				<footer class="site-footer" id="colophon">
 
-					<div class="site-info">
-
-						<?php understrap_site_info(); ?>
-
-					</div><!-- .site-info -->
+					<p class="text-end m-0">
+						Made with <i class="bi bi-heart-fill text-danger"></i> by <a href="https://www.linkedin.com/in/rajat-roy-574686201" target="_blank" rel="noopener noreferrer">Rajat Roy</a>
+					</p>
 
 				</footer><!-- #colophon -->
 
@@ -41,12 +40,13 @@ $container = get_theme_mod( 'understrap_container_type' );
 
 </div><!-- #wrapper-footer -->
 
-<?php // Closing div#page from header.php. ?>
+<?php // Closing div#page from header.php. 
+?>
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
 
+
 </body>
 
 </html>
-
